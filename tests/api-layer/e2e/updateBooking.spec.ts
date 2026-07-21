a544a3eeee11fedbc2297b61c48f445505e78de8
Bhavyasri <pillalamarribhavyasri214@gmail.com>
2026-07-21 11:42:41 +0530

    feat: enable nightly Playwright tests and add update bookings API validation tests

--- tests/api-layer/e2e/updateBooking.spec.ts
+++ tests/api-layer/e2e/updateBooking.spec.ts
@@ -28,3 +28,19 @@ test.describe('update bookings api',()=>{
         expect(responseBody.lastname).toBe(payload.createBookingPayload.lastname)
     })
 })
+
+test.describe('update bookings api', () => {
+   
+
+
+    test('invalid test response is 403', async ({ request }) => {
+        
+       const updateBookingApiResponse = await updateBookingAPi(request, payload.createBookingPayload,"2323434r45")
+        await expect(updateBookingApiResponse.status()).toBe(403)
+    })
+    test('empty token- response is 403', async ({ request }) => {
+
+        const updateBookingApiResponse = await updateBookingAPi(request, payload.createBookingPayload, "")
+        await expect(updateBookingApiResponse.status()).toBe(403)
+    })
+})
